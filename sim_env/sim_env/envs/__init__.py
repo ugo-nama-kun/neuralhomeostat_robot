daec04c331140eec14c16bb8d0b9dd310f41b923
Naoto Yoshida <n-yoshida@isi.imi.i.u-tokyo.ac.jp>
2026-03-06 18:34:30 +0900

    bugfix

--- sim_env/sim_env/envs/__init__.py
+++ sim_env/sim_env/envs/__init__.py
@@ -1,2 +1,2 @@
-from sim_env.envs.realant import RealAntPlayroomEnv, RealAntEnv, RealAntNavigationEnv, RealAntOnTheBoardEnv, RealAntCommandEnv
+from sim_env.envs.realant import RealAntPlayroomEnv, RealAntEnv, RealAntOnTheBoardEnv, RealAntCommandEnv
 from sim_env.envs.realant_base import RealAntBaseEnv, RealAntBaseOnTheBoardEnv, RealAntBaseCommandEnv, RealAntBasePlayroomEnv2
